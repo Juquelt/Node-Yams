7c782a806674853b6f60b2e47ab80042935ea862
Tiphaine JUQUEL <Tiphaine.juquel@gmail.com>
2023-05-24 10:15:37 +0200

    Add field js app

---
+++ app.js
@@ -0,0 +1,74 @@
+// routing on server side.
+const express = require("express");
+
+// Creating express object
+const app = express();
+
+// Require path module
+const path = require('path');
+
+// Require pug template
+const pug = require("pug");
+
+// Require mongoose to use mongoDb
+const mongoose = require("mongoose");
+const port = 3000;
+
+// Make a static route to use your
+// static files in client side
+app.use('/static', express.static('static'));
+
+// Middleware for parsing
+app.use(express.urlencoded());
+
+// Define and use pug engine so also
+// declare path on rendering
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
+// Database Connection
+mongoose.connect(
+    "mongodb://localhost:27017/feedback",
+    { useUnifiedTopology: true }
+);
+
+// Create schema
+const feedschema = mongoose.Schema({
+    name: String,
+    email: String,
+    feed: String
+});
+
+// Making a modal on our already
+// defined schema
+const feedModal = mongoose
+    .model('feeds', feedSchema);
+
+// Handling get request
+app.get('/', function (req, res) {
+    // Rendering your form
+    res.render('feedback_form');
+});
+
+// Handling data after submission of form
+app.post("/feedback_form", function (req, res) {
+    const feedData = new feedModal({
+        name: req.body.name,
+        email: req.body.email,
+        feed: req.body.feedback
+    });
+    feedData.save()
+        .then(data => {
+            res.render('feedback_form',
+                { msg: "Your feedback successfully saved." });
+        })
+        .catch(err => {
+            res.render('feedback_form',
+                { msg: "Check Details." });
+        });
+})
+
+// Server setup
+app.listen(port, () => {
+    console.log("server is running");
+});
